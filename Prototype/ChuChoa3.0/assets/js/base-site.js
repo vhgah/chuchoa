a8e602b76df99d62e7ad4a9ec41291624eb216dc
NguyenDuyCuong <cuongnd.ami@outlook.com>
2017-06-22 18:09:24 +0700

    reinit to use bootstrap-material-design

--- Prototype/ChuChoa3.0/assets/js/base-site.js
+++ Prototype/ChuChoa3.0/assets/js/base-site.js
@@ -1,13 +1,1 @@
-var DropDownNavigation = (function() {
-    function init(){
-        $(".chuchoa-navigation__dropdown-link").on('click', function(){            
-            $(this).addClass('selected');
-        });
-    }
-
-    init();
-
-    return {
-        init : init,
-    }
-})();
+$.material.init();
